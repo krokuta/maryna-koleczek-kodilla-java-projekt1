dd4e8cb24e28c360c0345ec896ade940dbdc5cf4
krokuta <marynako@interia.pl>
2019-11-12 11:25:12 +0100

    p1.1

---
+++ src/main/java/com/kodilla/project1kodillajava/Project1KodillaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.project1kodillajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1KodillaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project1KodillaJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/project1kodillajava/Project1KodillaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.project1kodillajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1KodillaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
